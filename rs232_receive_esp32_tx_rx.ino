a68f528d48c1baaa5ecee8ab85482409e9fcf518
Vũ Tuấn Anh <92041804+VuTuanAnh-1368@users.noreply.github.com>
2024-04-29 10:06:34 +0700

    Update rs232_receive_esp32_tx_rx.ino
    Update code

--- rs232_receive_esp32_tx_rx.ino
+++ rs232_receive_esp32_tx_rx.ino
@@ -6,21 +6,6 @@ void setup() {
 }
 
 void loop() {
-  // Send "Hello STM32" to the STM32
-  // Serial.println("Hello STM32");
-  Serial.println("Received: Hello Esp");
-  // Wait a little to receive data
-  delay(1000);
-
-
-
-
-
-
-
-
-
-
   // Check if data is available to read
   if (Serial.available() > 0) {
     String received = Serial.readString(); // Read the incoming data
